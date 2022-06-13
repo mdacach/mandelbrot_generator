49a6d291e7bc6c256351c38aaab0b96b26040048
Matheus Cardoso <dacachmatheus@gmail.com>
2022-06-13 20:50:35 -0300

    Add main code to create PNG image

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,10 @@
+use std::env;
 use std::fs::File;
 use std::str::FromStr;
 
 use image::png::PNGEncoder;
 use image::ColorType;
 use num::Complex;
-
-fn main() {
-    // square_loop(-2.1);
-    escape_time(
-        Complex {
-            re: 0.25,
-            im: -0.51,
-        },
-        1000,
-    );
-}
 
 /// Try to determine if `c` is in the Mandelbrot set, using at most `limit`
 /// iterations to decide.
@@ -178,4 +168,28 @@ fn write_image(
     encoder.encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
 
     Ok(())
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!(
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        );
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("Error parsing image dimensions.");
+    let upper_left = parse_complex(&args[3]).expect("Error parsing upper left corner point.");
+    let lower_right = parse_complex(&args[4]).expect("Error parsing lower right corner point.");
+
+    let total_pixels = bounds.0 * bounds.1;
+    let mut pixels = vec![0; total_pixels];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds).expect("Error writing PNG file.");
 }
