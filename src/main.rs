e330614319d950c7c562e36761b826037060075e
Matheus Cardoso <dacachmatheus@gmail.com>
2022-06-13 21:40:52 -0300

    Add concurrent render through crossbeam crate
    Make each thread work on a portion of the image.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,50 @@ use image::png::PNGEncoder;
 use image::ColorType;
 use num::Complex;
 
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!(
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        );
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("Error parsing image dimensions.");
+    let upper_left = parse_complex(&args[3]).expect("Error parsing upper left corner point.");
+    let lower_right = parse_complex(&args[4]).expect("Error parsing lower right corner point.");
+
+    let total_pixels = bounds.0 * bounds.1;
+    let mut pixels = vec![0; total_pixels];
+
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (index, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * index;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right =
+                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+
+                spawner.spawn(move |_| {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        })
+        .unwrap();
+    }
+
+    write_image(&args[1], &pixels, bounds).expect("Error writing PNG file.");
+}
+
 /// Try to determine if `c` is in the Mandelbrot set, using at most `limit`
 /// iterations to decide.
 ///
@@ -168,28 +212,4 @@ fn write_image(
     encoder.encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
 
     Ok(())
-}
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() != 5 {
-        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
-        eprintln!(
-            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
-            args[0]
-        );
-        std::process::exit(1);
-    }
-
-    let bounds = parse_pair(&args[2], 'x').expect("Error parsing image dimensions.");
-    let upper_left = parse_complex(&args[3]).expect("Error parsing upper left corner point.");
-    let lower_right = parse_complex(&args[4]).expect("Error parsing lower right corner point.");
-
-    let total_pixels = bounds.0 * bounds.1;
-    let mut pixels = vec![0; total_pixels];
-
-    render(&mut pixels, bounds, upper_left, lower_right);
-
-    write_image(&args[1], &pixels, bounds).expect("Error writing PNG file.");
 }
